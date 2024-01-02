935443106a4330ed122c2eed6d783c01615ffe61
daniel <daniel@daniel-inception-42.org>
2024-01-02 16:39:57 +0100

    Trying to test script sql

--- mariadb/tools/entry_point_mariadb.sh
+++ mariadb/tools/entry_point_mariadb.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
-echo "MySQL secure installation..."
+# Define the actual root password
+ROOT_PASSWORD="12345"
+
+# Path to the original SQL script
+SQL_SCRIPT="/secure_init_db.sql"
+
+# Create a temporary file to store modified SQL
+TEMP_SQL="/temp_secure_init_db.sql"
+
+# Replace placeholder with actual password in the SQL script
+sed "s/{{ROOT_PASSWORD}}/$ROOT_PASSWORD/g" "$SQL_SCRIPT" > "$TEMP_SQL"
+
+
+# # Run mysqld with the modified SQL script
+# mysqld --bootstrap --init-file="$TEMP_SQL"
+
+
+mysqld --bootstrap --init-file="$TEMP_SQL"
+rm "$TEMP_SQL"
+
+echo "Starting MariaDB service..."
 
 # Start MariaDB service
 service mariadb start
+
 
 # Wait for MariaDB service to start (optional)
 # This sleep command gives some time for MariaDB to start, adjust as needed
 sleep 10
 
-echo -e "1234\nn\ny\ny\ny\n" | mysql_secure_installation
+mysql -u root -p"12345" -e "SHOW DATABASES;"
 
-mysql -u root -p"1234" -e "
-        SHOW TABLES;\"
+# cat /etc/mysql/my.cnf
---
+++ mariadb/tools/secure_init_db.sql
@@ -0,0 +1,17 @@
+-- Set root password
+ALTER USER 'root'@'localhost' IDENTIFIED BY '{{ROOT_PASSWORD}}';
+
+-- Remove anonymous users
+DELETE FROM mysql.user WHERE User='';
+
+-- Disallow root login remotely
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+
+-- Remove test database
+DROP DATABASE IF EXISTS test;
+
+-- Remove privileges related to the test database
+DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
