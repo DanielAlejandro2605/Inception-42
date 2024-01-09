e3897aaf472f6de11497e3f239dd7d50fbb51b41
daniel <daniel@daniel-inception-42.org>
2024-01-09 14:24:25 +0100

    Adding WP config, more checking in WP entrypoint

--- srcs/requirements/mariadb/tools/entry_point_mariadb.sh
+++ srcs/requirements/mariadb/tools/entry_point_mariadb.sh
@@ -1,46 +1,46 @@
 #!/bin/bash
 
-# Path to the original SQL script
-SQL_SCRIPT="/secure_init_db.sql"
+# Checking whether database exists
+if [ ! -d "/var/lib/mysql/${DB_NAME}" ]; then
+    
+    # Path to the original SQL script
+    SQL_SCRIPT="/secure_init_db.sql"
 
-# Run mysqld with the modified SQL script
-mysqld --bootstrap --init-file="$SQL_SCRIPT"
+    # Run mysqld with the modified SQL script
+    mysqld --bootstrap --init-file="$SQL_SCRIPT"
 
-# Start MariaDB
-service mariadb start
-sleep 2
+    # Start MariaDB
+    service mariadb start
+    sleep 2
 
-# Create database for wordpress
-echo " (*) Creating ${DB_NAME} database ..."
-mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+    # Create database for wordpress
+    echo " (*) Creating ${DB_NAME} database ..."
+    mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 
-# Create user for wordpress database
-echo " (*) Creating user ${DB_USER_NAME} ... "
+    # Create user for wordpress database
+    echo " (*) Creating user ${DB_USER_NAME} ... "
+    mysql -e "CREATE USER '$DB_USER_NAME'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';"
 
-# mysql -e "DROP USER IF EXISTS '$WP_USERNAME';"
-# mysql -e "FLUSH PRIVILEGES;"
-mysql -e "CREATE USER '$DB_USER_NAME'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';"
+    # Grant privileges for wordpress user database to the wordpress database
+    echo " (*) Grant privileges for ${DB_USER_NAME} on ${DB_NAME} ... "
+    mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER_NAME'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';"
 
+    # Save the privileges 
+    echo " (*) Flush privileges ... "
+    mysql -e "FLUSH PRIVILEGES;"
 
-# Grant privileges for wordpress user database to the wordpress database
-echo " (*) Grant privileges for ${DB_USER_NAME} on ${DB_NAME} ... "
-mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER_NAME'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';"
+    # Set root password
+    echo " (*) Setting root password ... "
+    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_USER_PASSWORD';"
 
-# Save the privileges 
-echo " (*) Flush privileges ... "
-mysql -e "FLUSH PRIVILEGES;"
+    # Save the privileges
+    mysql -uroot -p${DB_ROOT_USER_PASSWORD} -e "FLUSH PRIVILEGES;"
 
-# Set root password
-echo " (*) Setting root password ... "
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_USER_PASSWORD';"
+    mysqladmin -uroot -p${DB_ROOT_USER_PASSWORD} shutdown
 
-# Save the privileges
-mysql -uroot -p${DB_ROOT_USER_PASSWORD} -e "FLUSH PRIVILEGES;"
-
-mysqladmin -uroot -p${DB_ROOT_USER_PASSWORD} shutdown
-
-echo "=> MariaDB database and user were created successfully! "
-
-# cat /etc/mysql/mariadb.conf.d/50-server.cnf
+    echo "=> MariaDB database and user were created successfully!"
+else
+    echo "Database is already setup!"
+fi
 
 exec mysqld
--- srcs/requirements/wordpress/tools/entry_point_wordpress.sh
+++ srcs/requirements/wordpress/tools/entry_point_wordpress.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-# DB_ROOT_PASSWORD=$MARIADB_ROOT_USER_PASSWORD
-# DB_NAME=$WP_DATABASE_NAME
-# DB_USER_NAME=$WP_USER_NAME
-# DB_USER_PASSWORD=$WP_USER_PASSWORD
-
-# echo $MARIADB_ROOT_USER_PASSWORD
-# echo $WP_DATABASE_NAME
-# echo $WP_USER_NAME
-# echo $WP_USER_NAME
-
 # Function to test if MariaDB is avalaible
 wait_for_mariadb() {
-    until mysqladmin ping -h"mariadb" -u"toto" -p"totoisthebest" &> /dev/null; do
+    until mysqladmin ping -h${DB_HOST} -u${DB_USER_NAME} -p${DB_USER_PASSWORD} &> /dev/null; do
         >&2 echo "MariaDB is unavailable - sleeping"
         sleep 2
     done
@@ -26,36 +16,36 @@ sleep 5
 
 cd $WP_PATH
 
-# Downloading wordpress:
-#   This command creates a  folder inside current working directory and downloads the latest WordPress version.
+if [ -d "$WP_PATH" ]; then
+    if [ ! "$(ls -A "$WP_PATH")" ]; then
+        # Downloading wordpress:
+        #   This command creates a  folder inside current working directory and downloads
+        echo "Downloading WordPress using wp-cli..."
+        wp-cli core download --path="$WP_PATH" --allow-root
+        echo "Done!"
+    fi
+fi
 
-echo "Downloading wordpress using wp-cli..."
-
-wp-cli core download --path=${WP_PATH} --allow-root
-
-echo "Done!"
-
-# Creating config file:
-#   Generate a config file wp-config.php for set up the database credentials for our installation.
-
-echo "Creating the config file for the database..."
-
-wp-cli config create    --dbname=${DB_NAME}                     \
-                        --dbuser=${DB_USER_NAME}                \
-                        --dbpass=${DB_USER_PASSWORD}            \
-                        --dbhost=${DB_HOST}                     \
-                        --path=${WP_PATH}                  \
-                        --allow-root
+if [ ! -f "${WP_PATH}/wp-config.php" ]; then
+    # Creating config file:
+    #   Generate a config file wp-config.php for set up the database credentials for our installation.
+    echo "Creating the config file for the database..."
+    wp-cli config create    --dbname=${DB_NAME}                     \
+                            --dbuser=${DB_USER_NAME}                \
+                            --dbpass=${DB_USER_PASSWORD}            \
+                            --dbhost=${DB_HOST}                     \
+                            --path=${WP_PATH}                  \
+                            --allow-root
+    echo "Done!"
+fi
 
 # Creating the site web
 echo "Creating the site web..."
-
 wp-cli core install     --url=${WP_DOMAIN_NAME}                 \
                         --title=${WP_TITLE}                     \
                         --admin_user=${WP_ADMIN_USER}           \
                         --admin_password=${WP_ADMIN_PASSWORD}   \
                         --admin_email=${WP_ADMIN_EMAIL}         \
                         --allow-root
-
 
 echo "Done!"
