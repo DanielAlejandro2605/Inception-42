b991c9f18557576a6f9dabdcb5667c19621458ba
daniel <daniel@daniel-inception-42.org>
2024-01-10 15:16:12 +0100

    Maybe first version of project:Testing outside of VM

--- srcs/requirements/wordpress/tools/entry_point_wordpress.sh
+++ srcs/requirements/wordpress/tools/entry_point_wordpress.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Path to the file which contains the list of downloaded files
+download_file_list_file="/tmp/download_file_list.txt"
+
 # Function to test if MariaDB is avalaible
 wait_for_mariadb() {
     until mysqladmin ping -h${DB_HOST} -u${DB_USER_NAME} -p${DB_USER_PASSWORD} &> /dev/null; do
@@ -16,65 +19,54 @@ sleep 5
 
 cd $WP_PATH
 
-list_files_installation="/tmp/list_file_install.txt"
 
-ls > /tmp/output_folder
+if [ ! -f "wp-config.php" ]; then
+    ls > /tmp/current_state.txt
+    # Downloading wordpress:
+    #   This command creates a  folder inside current working directory and downloads
+    if cmp --silent -- "/tmp/current_state.txt" "$download_file_list_file"; then
+      echo "The current status of the directory indicates that the download is done!"
+    else
+      echo "Downloading WordPress using wp-cli..."
+      wp-cli core download --path="$WP_PATH" --allow-root
+      echo "Done!"
+    fi
 
-if cmp --silent -- "/tmp/output_folder" "$list_files_installation"; then
-  echo "files contents are identical"
-else
-  echo "files differ"
+    # Creating config file:
+    #   Generate a config file wp-config.php for set up the database credentials for our installation.
+    echo "Creating the config file for the database..."
+    wp-cli config create    --dbname=${DB_NAME}                     \
+                            --dbuser=${DB_USER_NAME}                \
+                            --dbpass=${DB_USER_PASSWORD}            \
+                            --dbhost=${DB_HOST}                     \
+                            --path=${WP_PATH}                       \
+                            --allow-root
+    echo "Done!"
 fi
 
-cat /tmp/output_folder
+# Creating the site web
+echo "Creating the site web..."
+wp-cli core install     --url=${WP_DOMAIN_NAME}                 \
+                        --title=${WP_TITLE}                     \
+                        --admin_user=${WP_ADMIN_USER}           \
+                        --admin_password=${WP_ADMIN_PASSWORD}   \
+                        --admin_email=${WP_ADMIN_EMAIL}         \
+                        --allow-root
 
-echo "**************"
 
-cat $list_files_installation
+check=$(wp-cli user list --field=user_login --allow-root | grep $WP_USER)
+if [ $check == $WP_USER ]; then
+    echo "The user is already register!"
+else
+    # Creating WordPress user
+    echo "Creating new user ($WP_USER)..."
+    wp-cli user create ${WP_USER} ${WP_USER_EMAIL}                  \
+                            --user_pass=${WP_USER_PASSWORD}         \
+                            --role=${WP_USER_ROLE}                  \
+                            --allow-root
+    echo "Done!"
+fi
 
-rm /tmp/output_folder
-
-# if [ ! -f "wp-config.php" ]; then
-#     # Downloading wordpress:
-#     #   This command creates a  folder inside current working directory and downloads
-#     echo "Downloading WordPress using wp-cli..."
-#     wp-cli core download --path="$WP_PATH" --allow-root
-#     echo "Done!"
-# fi
-
-# ls 
-# if [ -f "wp-config.php" ]; then
-#     # Creating config file:
-#     #   Generate a config file wp-config.php for set up the database credentials for our installation.
-#     echo "Creating the config file for the database..."
-#     wp-cli config create    --dbname=${DB_NAME}                     \
-#                             --dbuser=${DB_USER_NAME}                \
-#                             --dbpass=${DB_USER_PASSWORD}            \
-#                             --dbhost=${DB_HOST}                     \
-#                             --path=${WP_PATH}                       \
-#                             --allow-root
-#     echo "Done!"
-# fi
-
-# # Creating the site web
-# echo "Creating the site web..."
-# wp-cli core install     --url=${WP_DOMAIN_NAME}                 \
-#                         --title=${WP_TITLE}                     \
-#                         --admin_user=${WP_ADMIN_USER}           \
-#                         --admin_password=${WP_ADMIN_PASSWORD}   \
-#                         --admin_email=${WP_ADMIN_EMAIL}         \
-#                         --allow-root
-
-# # Creating WordPress user
-# echo "Creating new user ($WP_USER)..."
-# wp-cli user create ${WP_USER} ${WP_USER_EMAIL}                  \
-#                         --user_pass=${WP_USER_PASSWORD}         \
-#                         --role=${WP_USER_ROLE}                  \
-#                         --allow-root
-# echo "Done!"
-
-# echo "Activating WordPress theme..."
-
-# wp-cli theme activate twentytwentythree --allow-root
+wp-cli theme activate twentytwentythree --allow-root
 
 php-fpm7.4 -F -R
